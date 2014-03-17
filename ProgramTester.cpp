3dd73d154a2a4bf2d050e21756fed203b2ad5cfb
Erik Hattervig <erik.hattervig@gmail.com>
2014-03-16 21:43:15 -0600

    Changed Function Headers to closer to Doxygen Style
    It was bugging me and hard to read the other way.

--- ProgramTester.cpp
+++ ProgramTester.cpp
@@ -1,26 +1,26 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Simple Program Tester
-//
-//  Authors: Colter Assman, Samuel Carroll, Shaun Gruenig
-//  CS470 - Software Engineering
-//  Dr. Logar
-//  February 19
-//
-//  Program Description:
-//      This program is to be placed into a directory containing
-//      subdirectories with .cpp files, associated .tst files which
-//      contain test input for the program contained in the cpp file,
-//      and .ans files that contain the expected output associated with
-//      said input. This program will then find the specified cpp file,
-//      compile and run the program against all given test cases, and
-//      output a log file in the same directory as the .cpp file.
-//
-//      A more in-depth description of this program can be found in
-//      the accompanying documentation.
-//
-//
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ *  Simple Program Tester
+ *
+ *  Authors: Colter Assman, Samuel Carroll, Shaun Gruenig
+ * CS470 - Software Engineering
+ * Dr. Logar
+ * February 19
+ *
+ * Program Description:
+ *     This program is to be placed into a directory containing
+ *     subdirectories with .cpp files, associated .tst files which
+ *     contain test input for the program contained in the cpp file,
+ *     and .ans files that contain the expected output associated with
+ *     said input. This program will then find the specified cpp file,
+ *   compile and run the program against all given test cases, and
+ *    output a log file in the same directory as the .cpp file.
+ *
+ *     A more in-depth description of this program can be found in
+ *     the accompanying documentation.
+ *
+ *
+******************************************************************************/
 
 
 /////////includes//////////////////////////////////////////////////////////////
@@ -60,26 +60,27 @@ void StudentLogWrite( std::ofstream &fout, string testName, bool passedStatus );
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-//
-//  main function
-//
-//  this function is the main entryway into the program. It initializes
-//  all necessary variables and calls all necessary functions to accomplish
-//  the tasks of the program.  It calls the parse_directory function to
-//  find the target cpp file, which in turn calls the find_tst function
-//  to find all .tst files associated with that target and run the test
-//  inputs against the target program. Main() then compiles all the
-//  statistics recorded during the tests and outputs the stats to a log
-//  file.
-//
-//  Parameters:
-//      int argc - number of arguments passed in
-//      char* argv[] - array of c strings containing CL arguments
-//
-//  Author: Shaun Gruenig
-//
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ *  main function
+ *
+ *  @Description:
+ *  this function is the main entryway into the program. It initializes
+ *  all necessary variables and calls all necessary functions to accomplish
+ *  the tasks of the program.  It calls the parse_directory function to
+ *  find the target cpp file, which in turn calls the find_tst function
+ *  to find all .tst files associated with that target and run the test
+ *  inputs against the target program. Main() then compiles all the
+ *  statistics recorded during the tests and outputs the stats to a log
+ *  file.
+ *
+ *  Parameters:
+ *      int argc - number of arguments passed in
+ *      char* argv[] - array of c strings containing CL arguments
+ *
+ *  @Author: Shaun Gruenig
+ *
+******************************************************************************/
 int main ( int argc, char* argv[] )
 {
     ofstream fout; //file stream for .log file
@@ -115,26 +116,27 @@ int main ( int argc, char* argv[] )
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-//
-//  parse_directory function
-//
-//  This function searches through the directory and sub-directories that
-//  the program was run from until it finds a .cpp file that has
-//  a file name matching the one specified by the user. It then compiles
-//  the .cpp file, opens a related log file and calls the find_tst
-//  function to locate the tests associated with that target program
-//
-//  Parameters:
-//      string root - contains name of current root directory
-//      string target - contains the file name specified by the user
-//      ofstream &fout - file stream for writting to log file
-//      data_struct* test_stats - contains pointer to a structure
-//          which contains collected statistics from tests
-//
-//  Authors: Colter Assman, Shaun Gruenig
-//
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ *  parse_directory function
+ *
+ *  @Description:
+ *  This function searches through the directory and sub-directories that
+ *  the program was run from until it finds a .cpp file that has
+ *  a file name matching the one specified by the user. It then compiles
+ *  the .cpp file, opens a related log file and calls the find_tst
+ *  function to locate the tests associated with that target program
+ *
+ *  Parameters:
+ *      string root - contains name of current root directory
+ *      string target - contains the file name specified by the user
+ *      ofstream &fout - file stream for writting to log file
+ *      data_struct* test_stats - contains pointer to a structure
+ *          which contains collected statistics from tests
+ *
+ *  @Authors: Colter Assman, Shaun Gruenig
+ *
+******************************************************************************/
 void parse_directory ( string root, int level, string target, ofstream &fout,
                        data_struct* test_stats )
 {
@@ -205,28 +207,29 @@ void parse_directory ( string root, int level, string target, ofstream &fout,
 
 }
 
-///////////////////////////////////////////////////////////////////////////////
-//
-//  find_tst function
-//
-//  This function searches through the directory and sub-directories that
-//  the program was run from until it finds a .cpp file that has
-//  a file name matching the one specified by the user. It then compiles
-//  the .cpp file, opens a related log file and calls the find_tst
-//  function to locate the tests associated with that target program
-//
-//  Parameters:
-//      string root - contains name of directory containing the
-//          the program we are testing
-//      string curr_dir - contains name of current directory
-//      string target - contains the file name specified by the user
-//      ofstream &fout - file stream for writting to log file
-//      data_struct* test_stats - contains pointer to a structure
-//          which contains collected statistics from tests
-//
-//  Authors: Colter Assman, Shaun Gruenig
-//
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ *  find_tst function
+ *
+ *  @Description:
+ *  This function searches through the directory and sub-directories that
+ *  the program was run from until it finds a .cpp file that has
+ *  a file name matching the one specified by the user. It then compiles
+ *  the .cpp file, opens a related log file and calls the find_tst
+ *  function to locate the tests associated with that target program
+ *
+ *  Parameters:
+ *      string root - contains name of directory containing the
+ *          the program we are testing
+ *      string curr_dir - contains name of current directory
+ *      string target - contains the file name specified by the user
+ *      ofstream &fout - file stream for writting to log file
+ *      data_struct* test_stats - contains pointer to a structure
+ *          which contains collected statistics from tests
+ *
+ *  @Authors: Colter Assman, Shaun Gruenig
+ *
+******************************************************************************/
 void find_tst ( string root, string curr_dir, string target, ofstream &fout, 
 		data_struct* test_stats )
 {
@@ -313,23 +316,24 @@ void find_tst ( string root, string curr_dir, string target, ofstream &fout,
 }
 
 
-///////////////////////////////////////////////////////////////////////////////
-//
-//  run_diff function
-//
-//  This function will build a diff command for the system function. After that
-//  command is built we will run that command and if there is no difference
-//  we return true, and if there is a difference we return false. This will be
-//  used to determine if the program result passed or failed, and then use the
-//  information to write to the log file.
-// 
-//  Parameters:
-//	string file1 - file to be compared with file2
-//	string file2 - file to be compared with file1
-//
-//  Author: Samuel Carroll
-//
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ *  run_diff function
+ *
+ *  @Description:
+ *  This function will build a diff command for the system function. After that
+ *  command is built we will run that command and if there is no difference
+ *  we return true, and if there is a difference we return false. This will be
+ *  used to determine if the program result passed or failed, and then use the
+ *  information to write to the log file.
+ * 
+ *  Parameters:
+ *	string file1 - file to be compared with file2
+ *	string file2 - file to be compared with file1
+ *
+ *  @Author: Samuel Carroll
+ *
+******************************************************************************/
 
 bool run_diff ( string file1, string file2 )
 {
@@ -354,18 +358,19 @@ bool run_diff ( string file1, string file2 )
 } // end of run_diff function
 
 
-///////////////////////////////////////////////////////////////////////////////
-//
-//  get_time function
-//
-//  This function uses the <time.h> library functions to construct
-//  a string which contains the time and date
-//
-//  Parameters: none
-//
-//  Author: Shaun Gruenig (referred to cplusplus.com for example code)
-//
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ *  get_time function
+ *
+ *  @Description:
+ *  This function uses the <time.h> library functions to construct
+ *  a string which contains the time and date
+ *
+ *  Parameters: none
+ *
+ *  @Author: Shaun Gruenig (referred to cplusplus.com for example code)
+ *
+******************************************************************************/
 string get_time ()
 {
     char string_version[1000]; //stores date and time
@@ -380,26 +385,26 @@ string get_time ()
     return ( string ) string_version;
 }
 
-///////////////////////////////////////////////////////////////////////////////
-//
-//FinalLogWrite
-//
-//@Author: Jonathan Tomes
-//
-//@Description:
-// This function will write the final log entry to a student file. or to the
-// class log file. It expects the stream to write to, the students name
-// and the number of tests their program passed, and the number o tests
-// actually used to test their code. If the number of tests passed is zero
-// it assumes a critical test was failed so instead of printing the percent
-// of passed tests it just prints "FAILED" next to their name.
-//
-// @param[in] fout - the stream to write to.
-// @param[in] name - the students name.
-// @param[in] numPassed - the number of tests passed, if zero assumes failed
-//      critical test.
-// @Param[in] numTotal - the total number of tests.
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ * FinalLogWrite
+ *
+ * @Author: Jonathan Tomes
+ *
+ * @Description:
+ *  This function will write the final log entry to a student file. or to the
+ *  class log file. It expects the stream to write to, the students name
+ *  and the number of tests their program passed, and the number o tests
+ *  actually used to test their code. If the number of tests passed is zero
+ *  it assumes a critical test was failed so instead of printing the percent
+ *  of passed tests it just prints "FAILED" next to their name.
+ *
+ *  @param[in] fout - the stream to write to.
+ *  @param[in] name - the students name.
+ *  @param[in] numPassed - the number of tests passed, if zero assumes failed
+ *      critical test.
+ *  @Param[in] numTotal - the total number of tests.
+******************************************************************************/
 
 void FinalLogWrite( std::ofstream &fout, string name, int numPassed, 
         int numTotal )
@@ -422,19 +427,19 @@ void FinalLogWrite( std::ofstream &fout, string name, int numPassed,
     return;
 }
 
-///////////////////////////////////////////////////////////////////////////////
-//
-//@Author: Jonathan Tomes
-//
-//@Description:
-//  Prints the status of a test to the file stream. Status should be weather
-// or not the a test passed (true) or failed (false).
-//
-//@param[in] fout - the stream to write to.
-//@param[in] testName - name of the test.
-//@param[in] passedStatus - true - test passed.
-//                          false - test failed.
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ * @Author: Jonathan Tomes
+ *
+ * @Description:
+ *  Prints the status of a test to the file stream. Status should be weather
+ * or not the a test passed (true) or failed (false).
+ *
+ * @param[in] fout - the stream to write to.
+ * @param[in] testName - name of the test.
+ * @param[in] passedStatus - true - test passed.
+ *                          false - test failed.
+******************************************************************************/
 
 void StudentLogWrite( std::ofstream &fout, string testName, bool passedStatus )
 {
